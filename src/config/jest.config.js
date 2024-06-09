e9ba29864d20073143ad1f005c3065a3e4fd44db
mfaqihwijaya <faqih.wijaya@bithealth.co.id>
2024-06-10 06:36:31 +0700

    fix test coverage

--- src/config/jest.config.js
+++ src/config/jest.config.js
@@ -2,5 +2,18 @@ const config = {
     rootDir: '../__tests__/tests',
     collectCoverage: true,
     coverageDirectory: '../coverage',
+    coveragePathIgnorePatterns: [
+        '/node_modules/',
+        '/__tests__/',
+        'app.js',
+    ],
+    coverageThreshold: {
+        global: {
+            branches: 90,
+            functions: 90,
+            lines: 90,
+            statements: 90
+        }
+    }
 }
 module.exports = config
